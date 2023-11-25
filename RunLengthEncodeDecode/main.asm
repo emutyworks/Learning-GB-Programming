e3f5d3e4486548fbe93213943a67c0a3bb52fcf5
emutyworks <emuty@emutyworks.com>
2023-11-25 19:31:14 +0900

    Added RunLengthEncodeDecode

---
+++ RunLengthEncodeDecode/main.asm
@@ -0,0 +1,187 @@
+;
+; I used this Website/Document as a reference to create it.
+;
+; Run-length encoding
+; https://en.wikipedia.org/wiki/Run-length_encoding
+;
+; Lesson H9- Hello World on the Gameboy and Gameboy Color
+; https://www.chibiakumas.com/z80/helloworld.php#LessonH9
+;
+; Pan Docs
+; https://gbdev.io/pandocs/
+;
+
+INCLUDE "hardware.inc"
+
+SECTION "Header",ROM0[$100]
+
+EntryPoint:
+	di
+	jr Start
+
+REPT $150 - $104
+	db 0
+ENDR
+
+SECTION "Start",ROM0[$150]
+
+Start:
+	call WaitVBlank
+
+	; Set BG Palette
+	ld a,%10000000 ; Palette 0, Auto increment after writing
+	ldh [rBCPS],a
+	ld c,2
+	ld hl,BGPalette
+	ld de,rBCPD
+	call SetPalette
+
+	xor a
+	ldh [rLCDC],a
+	ldh [rIE],a
+	ldh [rIF],a
+	ldh [rSTAT],a
+	ldh [rSVBK],a
+	ldh [rSCY],a
+	ldh [rSCX],a
+
+	; Set Tiles data
+	ld hl,_VRAM+$1000 ;$9000
+	ld de,Tiles
+	ld bc,TilesEnd - Tiles
+	call CopyData
+
+	; Set Map data (Decompression version)
+	ld a,1
+	ldh [rVBK],a ; BG Map Attributes
+	ld hl,_SCRN0 ; write address
+	ld de,BgTileMap1 ; data address
+	ld bc,BgTileMap1End ; data end address
+	call CopyDecompressionData
+	xor a
+	ldh [rVBK],a ; Tile Indexes
+	ld hl,_SCRN0
+	ld de,BgTileMap0
+	ld bc,BgTileMap0End
+	call CopyDecompressionData
+
+	; Set Map data (No decompression version)
+	;ld a,1
+	;ldh [rVBK],a ; BG Map Attributes
+	;ld hl,_SCRN0
+	;ld de,BgTileMap1
+	;ld bc,BgTileMap1End - BgTileMap1
+	;call CopyData
+	;xor a
+	;ldh [rVBK],a ; Tile Indexes
+	;ld hl,_SCRN0
+	;ld de,BgTileMap0
+	;ld bc,BgTileMap0End - BgTileMap0
+	;call CopyData
+
+	ld a,LCDCF_ON|LCDCF_BGON
+	ldh [rLCDC],a
+
+MainLoop:
+	jp MainLoop
+
+; *This code has not been optimized yet. =D
+CopyDecompressionData:
+	;ld hl,<write address>
+	;ld de,<data address>
+	;ld bc,<data end address>
+	ld a,[de]
+	inc de
+	ld [hli],a
+.start
+	push bc
+.loop
+	ld b,a
+	ld a,[de]
+	cp b
+	jr z,.next
+	inc de
+	ld [hli],a
+	jr .loop
+.next
+	inc de
+	ld [hli],a
+	ld a,[de]
+	inc de
+	sub 2
+	cp 0
+	jr z,.skip
+	ld c,a
+	ld a,b
+.copy
+	ld [hli],a
+	dec c
+	jr nz,.copy
+.skip
+	pop bc
+	ld a,b
+	cp d
+	dec l
+	ld a,[hl]
+	inc l
+	jr nz,.start
+	ld a,c
+	cp e
+	dec l
+	ld a,[hl]
+	inc l
+	jr nz,.start
+	ret
+
+WaitVBlank:
+.loop
+	ldh a,[rLY]
+	cp SCRN_Y ; 144 ; Check if the LCD is past VBlank
+	jr nz,.loop
+	ret
+
+SetPalette:
+.loop
+	ld a,[hli]
+	ld [de],a
+	ld a,[hli]
+	ld [de],a
+	dec c
+	jr nz,.loop
+	ret
+
+CopyData:
+	ld a,[de] ; Grab 1 byte from the source
+	ld [hli],a ; Place it at the destination, incrementing hl
+	inc de ; Move to next byte
+	dec bc ; Decrement count
+	ld a,b ; Check if count is 0, since `dec bc` doesn't update flags
+	or c
+	jr nz,CopyData
+	ret
+
+BGPalette:
+	dw 0
+	dw 8456
+	dw 24311
+	dw 32767
+
+	dw 0
+	dw 8456
+	dw 32767
+	dw 24311
+
+SECTION "BG Data",ROM0[$2000]
+BgTileMap0: ; Tile Indexes
+	;INCBIN "bg_tile0_1024.bin" ; 1024 bytes
+	INCBIN "bg_tile0_compress.bin" ; 337 bytes
+BgTileMap0End:
+
+BgTileMap1: ; BG Map Attributes
+	;INCBIN "bg_tile1_1024.bin" ; 1024 bytes
+	INCBIN "bg_tile1_compress.bin" ; 151 bytes
+BgTileMap1End:
+
+Tiles:
+	INCBIN "tiles.bin"
+TilesEnd:
---
+++ RunLengthEncodeDecode/test_RunLengthDecoder.php
@@ -0,0 +1,56 @@
+<?php
+//==========================================================
+// Run-length Decoder
+// *This is a test implementation for data comparison.
+// 
+// https://en.wikipedia.org/wiki/Run-length_encoding
+//==========================================================
+
+if(count($argv)<3){
+  echo "Usage: php test_RunLengthDecoder.php <input filename(compression)> <output filename(bin)>\n";
+  exit;
+}
+$inpit_file = $argv[1];
+$output_file = $argv[2];
+
+$in_array = str_split(bin2hex(file_get_contents($inpit_file)),2);
+
+$val_past = array_shift($in_array);
+$val_next = null;
+$cnt = 0;
+$out_array = array();
+do{
+  echo $val_past;
+  $out_array[] = $val_past;
+  $val_next = array_shift($in_array);
+  if($val_past!=$val_next){
+    $val_past = $val_next;
+    continue;
+  }
+  echo $val_next;
+  $out_array[] = $val_next;
+  $cnt = hexdec(array_shift($in_array));
+  echo "|$cnt:";
+  for($i=0; $i<($cnt-2); $i++){
+    echo $val_next;
+    $out_array[] = $val_next;
+  }
+  echo "\n";
+  $val_past = array_shift($in_array);
+}while(count($in_array)!=0);
+
+//var_dump($out_array);
+//var_dump(count($out_array));
+
+$bin = null;
+for($i=0; $i<count($out_array); $i++){
+  $bin .= pack("c",hexdec($out_array[$i]));
+}
+file_put_contents($output_file, $bin);
+
+/*
+php test_RunLengthDecoder.php bg_tile0_compress.bin test_1024.bin
+
+*/
+
+?>
